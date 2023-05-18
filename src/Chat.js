c93ec2591807aa28d546fae1c1533bb5a287d500
Sam-Haider <sammhaider0@gmail.com>
2023-05-18 12:47:56 -0400

    Set up a basic Q&A for JS/React questions

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
 
@@ -7,12 +7,12 @@ const MainContainer = styled.div`
 `;
 
 const Chat = () => {
-  const [message, setMessage] = useState("");
-  const [response, setResponse] = useState("");
-  const [topic, setTopic] = useState("javascript"); // "javascript", "react", or "both"
-  const [level, setLevel] = useState("beginner"); // "beginner", "intermediate", or "advanced"
-  const [mode, setMode] = useState("learning topic"); // "learning topic" or "quiz question"
-  const [streak, setStreak] = useState(0); // keep track of correct answers in a row
+  const [topic, setTopic] = useState("javascript");
+  const [level, setLevel] = useState("beginner");
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [feedback, setFeedback] = useState("");
+  const [score, setScore] = useState(0);
 
   const handleTopicChange = (event) => {
     setTopic(event.target.value);
@@ -22,26 +22,19 @@ const Chat = () => {
     setLevel(event.target.value);
   };
 
-  const handleModeChange = (event) => {
-    setMode(event.target.value);
+  const handleAnswerChange = (event) => {
+    setAnswer(event.target.value);
   };
 
-  const resetStreak = () => {
-    setStreak(0);
-  };
-
-  const incrementStreak = () => {
-    setStreak((prevStreak) => prevStreak + 1);
-  };
-
-  const sendMessage = async () => {
+  const getQuizQuestion = async () => {
+    setFeedback("");
     try {
       const payload = {
         model: "gpt-3.5-turbo",
         messages: [
           {
             role: "user",
-            content: `Imagine you're a virtual tutor. Give me a ${level} ${mode} on ${topic}`,
+            content: `Imagine you're a virtual tutor. Give me a ${level} quiz question on ${topic}.`,
           },
         ],
       };
@@ -58,55 +51,89 @@ const Chat = () => {
       );
 
       const assistantMessage = response.data.choices[0].message.content.trim();
-      setResponse(assistantMessage);
-      setMessage("");
+      setQuestion(assistantMessage);
     } catch (error) {
       console.error(error);
     }
   };
 
-  console.log({ streak, incrementStreak, resetStreak });
+  // This will run when the component mounts
+  useEffect(() => {
+    getQuizQuestion();
+  }, []);
+
+  const assessAnswer = async () => {
+    try {
+      const payload = {
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: `Score this answer to the following ${level} ${topic} question, '${question}', on a scale of 0 to 100 and provide feedback. Answer: '${answer}'. In your response, start with the score, then </>, then the rest of your response. For example: 99</>Great job!`,
+          },
+        ],
+      };
+
+      const response = await axios.post(
+        "https://api.openai.com/v1/chat/completions",
+        payload,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.REACT_APP_OPENAI}`, // Replace with your actual API key
+          },
+        }
+      );
+
+      const assistantMessage = response.data.choices[0].message.content.trim();
+      // This assumes the AI returns the score as the first part of its response, followed by the feedback
+      console.log({ assistantMessage });
+      const [score, ...feedback] = assistantMessage.split("</>");
+      setScore(Number(score.trim()));
+      setFeedback(feedback.join("."));
+      setAnswer(""); // Reset the answer
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  console.log({ score });
 
   return (
     <MainContainer>
       <h1>ChatGPT</h1>
+
       <label>Topic: </label>
       <select value={topic} onChange={handleTopicChange}>
         <option value="javascript">JavaScript</option>
         <option value="react">React</option>
-        <option value="both">Both</option>
       </select>
+
       <label>Level: </label>
       <select value={level} onChange={handleLevelChange}>
         <option value="beginner">Beginner</option>
         <option value="intermediate">Intermediate</option>
         <option value="advanced">Advanced</option>
       </select>
-      <label>Mode: </label>
-      <select value={mode} onChange={handleModeChange}>
-        <option value="learning topic">Learning Topic</option>
-        <option value="quiz question">Quiz Question</option>
-      </select>
-      <button onClick={sendMessage}>Send</button>
-      <p>Streak: {streak}</p>
-      {mode === "quiz question" && (
+
+      <button onClick={getQuizQuestion}>Get Another Question</button>
+
+      {question && (
         <>
           <h2>Question:</h2>
-          <p>{response}</p>
+          <p>{question}</p>
 
           <h2>Your Answer:</h2>
-          <input
-            type="text"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-          <button onClick={sendMessage}>Submit Answer</button>
+          <input type="text" value={answer} onChange={handleAnswerChange} />
+          <button onClick={assessAnswer}>Submit Answer</button>
         </>
       )}
-      {mode === "learning topic" && (
+
+      {feedback && (
         <>
-          <h2>Learning Topic:</h2>
-          <p>{response}</p>
+          <h2>Feedback:</h2>
+          <p>{feedback}</p>
+          <p>Score: {score}</p>
         </>
       )}
     </MainContainer>
