00cec53a380fc10e5c5f7a384064873637aa2052
Sam-Haider <sammhaider0@gmail.com>
2023-05-18 13:29:00 -0400

    Styling

--- src/Chat.js
+++ src/Chat.js
@@ -3,7 +3,77 @@ import axios from "axios";
 import styled from "styled-components";
 
 const MainContainer = styled.div`
-  // Style your main container here
+  margin: 0 auto;
+  max-width: 800px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  min-height: 100vh;
+  background-color: #1a1a2e;
+  color: #fff;
+
+  h1 {
+    color: #78dad3;
+    font-size: 2rem;
+  }
+
+  label {
+    color: #adfffe;
+    margin-right: 10px;
+  }
+
+  select,
+  input[type="text"] {
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    background-color: #16213e;
+    color: #fff;
+    margin-bottom: 20px;
+    width: 100%;
+  }
+
+  button {
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    background-color: #0f3460;
+    color: #fff;
+    cursor: pointer;
+    margin-bottom: 20px;
+  }
+
+  .submit-answer-button {
+    background-color: #ff68a3;
+  }
+
+  .subheading {
+    margin: 0 0 40px 0;
+    padding: 0;
+  }
+`;
+
+const QuestionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #0f3460;
+  padding: 20px;
+  border-radius: 10px;
+  width: 96%;
+  max-width: 750px;
+  margin-bottom: 20px;
+
+  h2 {
+    color: #ff68a3;
+    margin-bottom: 10px;
+  }
+`;
+
+const FeedbackContainer = styled(QuestionContainer)`
+  background-color: #16213e;
 `;
 
 const Chat = () => {
@@ -101,15 +171,16 @@ const Chat = () => {
 
   return (
     <MainContainer>
-      <h1>ChatGPT</h1>
+      <h1>React and JS Quizzer</h1>
+      <p className="subheading">Powered by OpenAI's GPT-3.5 Turbo</p>
 
-      <label>Topic: </label>
+      <label>Topic </label>
       <select value={topic} onChange={handleTopicChange}>
         <option value="javascript">JavaScript</option>
         <option value="react">React</option>
       </select>
 
-      <label>Level: </label>
+      <label>Level </label>
       <select value={level} onChange={handleLevelChange}>
         <option value="beginner">Beginner</option>
         <option value="intermediate">Intermediate</option>
@@ -119,22 +190,24 @@ const Chat = () => {
       <button onClick={getQuizQuestion}>Get Another Question</button>
 
       {question && (
-        <>
+        <QuestionContainer>
           <h2>Question:</h2>
           <p>{question}</p>
 
           <h2>Your Answer:</h2>
           <input type="text" value={answer} onChange={handleAnswerChange} />
-          <button onClick={assessAnswer}>Submit Answer</button>
-        </>
+          <button className="submit-answer-button" onClick={assessAnswer}>
+            Submit Answer
+          </button>
+        </QuestionContainer>
       )}
 
       {feedback && (
-        <>
+        <FeedbackContainer>
           <h2>Feedback:</h2>
           <p>{feedback}</p>
           <p>Score: {score}</p>
-        </>
+        </FeedbackContainer>
       )}
     </MainContainer>
   );
